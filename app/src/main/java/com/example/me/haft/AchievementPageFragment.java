c14396ff9ee67f7b01d7875e91b625b45f24d913
Me <gryfxen@gmail.com>
2018-07-26 18:25:25 +0430

    achievements page implemented

--- app/src/main/java/com/example/me/haft/AchievementPageFragment.java
+++ app/src/main/java/com/example/me/haft/AchievementPageFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,10 +27,16 @@ public class AchievementPageFragment extends android.support.v4.app.Fragment {
         ArrayList<Item> items=new ArrayList<Item>();
         items.add(new SectionItem("7 MONTH CHALLENGE"));
         items.add(new EntryItem("Unbreak My Heart","restore your hearts by missing less than 3 workout days in a month",R.drawable.baseline_mood_black_18dp,false));
+        items.add(new EntryItem("Creating A Habit","Stay on the 7 Month Challenge for 3 months",R.drawable.baseline_golf_course_black_18dp,false));
+        items.add(new EntryItem("So Close I Can Taste It","Stay on the 7 Month Challenge for 6 months",R.drawable.baseline_directions_bike_black_18dp,true));
+        items.add(new EntryItem("7 Month Champion","Complete the 7 Month Challenge",R.drawable.baseline_rowing_black_18dp,false));
+        items.add(new SectionItem("CONSECUTIVE DAYS"));
+        items.add(new EntryItem("Challenge Accepted","Finnish your first workout",R.drawable.baseline_smoking_rooms_black_18dp,true));
+
 
         AchievementsAdapter adapter=new AchievementsAdapter(getActivity(),items);
         listView.setAdapter(adapter);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return rootView;
     }
 }
--- app/src/main/java/com/example/me/haft/achievementslist/AchievementsAdapter.java
+++ app/src/main/java/com/example/me/haft/achievementslist/AchievementsAdapter.java
@@ -3,10 +3,12 @@ package com.example.me.haft.achievementslist;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
@@ -15,45 +17,73 @@ import com.example.me.haft.R;
 
 import java.util.ArrayList;
 
-public class AchievementsAdapter extends ArrayAdapter{
+public class AchievementsAdapter extends BaseAdapter{
     private Context context;
     private ArrayList<Item> items;
     private LayoutInflater inflater;
 
     public AchievementsAdapter(Context context,ArrayList<Item> items){
-        super(context,0,items);
         this.context=context;
         this.items=items;
         inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        int type=1;
+        if (items.get(position).isSection()){
+            type=0;
+        }
+        return type;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view=convertView;
-        Item item=items.get(position);
-        if(item!=null){
-            if(item.isSection()){
-                SectionItem sectionItem=(SectionItem) item;
-                view=inflater.inflate(R.layout.achievement_section,null);
-                TextView titleView= (TextView) view.findViewById(R.id.section_title);
+        if (convertView==null){
+            if (getItemViewType(position)==0){
+                SectionItem sectionItem=(SectionItem) items.get(position);
+                convertView=inflater.inflate(R.layout.achievement_section,null);
+                TextView titleView= (TextView) convertView.findViewById(R.id.section_title);
                 titleView.setText(sectionItem.getTitle());
             }
             else{
-                EntryItem entryItem=(EntryItem)item;
-                view=inflater.inflate(R.layout.achievement_item,null);
-                TextView titleView= (TextView) view.findViewById(R.id.achievement_title);
-                TextView descriptionView= (TextView) view.findViewById(R.id.achievement_description);
-                ImageView iconView= (ImageView) view.findViewById(R.id.achievement_icon);
+                EntryItem entryItem=(EntryItem)items.get(position);
+                convertView=inflater.inflate(R.layout.achievement_item,null);
+                TextView titleView= (TextView) convertView.findViewById(R.id.achievement_title);
+                TextView descriptionView= (TextView) convertView.findViewById(R.id.achievement_description);
+                ImageView iconView= (ImageView) convertView.findViewById(R.id.achievement_icon);
                 titleView.setText(entryItem.getTitle());
                 descriptionView.setText(entryItem.getDescription());
                 iconView.setImageResource(entryItem.getIconId());
                 if (entryItem.isUnlocked()){
-                    view.findViewById(R.id.unlocked_icon).setVisibility(View.VISIBLE);
+                    convertView.findViewById(R.id.unlocked_icon).setVisibility(View.VISIBLE);
                 }
             }
         }
+        //TODO: decrease inflates
         //TODO: set view clickable
-        return view;
+        Log.d("debug","achvtAdapter.java>>getview called, position:"+position);
+        return convertView;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
     }
 }
